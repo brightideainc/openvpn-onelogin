875186ae3fdb11c135b9ab101284dc5de5c698f3
Feliks Beygel <fbeygel@brightidea.com>
2018-08-10 11:33:54 -0400

    Added support for static challenge

--- src/Authorize.php
+++ src/Authorize.php
@@ -44,6 +44,7 @@ class Authorize {
      *
      * @param array $args
      * @return void
+     * @throws AuthorizationException
      */
     private function validateArgs(array $args = []) : void {
         if (!isset($args[1]) || !file_exists($args[1])) {
@@ -65,6 +66,7 @@ class Authorize {
      *
      * @param array $args
      * @return void
+     * @throws AuthorizationException
      */
     private function parseAuthArgs(array $args) : void {
         try {
@@ -84,7 +86,10 @@ class Authorize {
             throw new AuthorizationException("Unable to load login credentials from OpenVPN");
         }
 
-        if (self::MFA_AUTH_REQUIRED === true) {
+        if($staticChallenge = $this->parseStaticChallengePassword($authArgs)) {
+            $authArgs[1] = $staticChallenge[0];
+            $this->setAuthArgs($authArgs, null, $staticChallenge[1]);
+        } else if (self::MFA_AUTH_REQUIRED === true) {
             if (preg_match('/(\-[0-9]+)$/', $authArgs[1], $matches)) {
                 $this->setAuthArgs($authArgs, $matches);
             } else {
@@ -95,20 +100,30 @@ class Authorize {
         }
     }
 
+    private function parseStaticChallengePassword(array $authArgs) : ?array {
+        $password = explode(':', $authArgs[1]);
+        if($password[0] !== 'SCRV1' ) {
+            return null;
+        }
+
+        return [base64_decode($password[1]), base64_decode($password[2])];
+    }
+
     /**
      * Undocumented function
      *
      * @param array $authArgs
      * @param array|null $matches
+     * @param string|null $mfa
      * @return void
      */
-    private function setAuthArgs(array $authArgs, ?array $matches = null) : void {
-        self::$authArgs = new class($authArgs, $matches) {
+    private function setAuthArgs(array $authArgs, ?array $matches = null, ?string $mfa = null) : void {
+        self::$authArgs = new class($authArgs, $matches, $mfa) {
             protected $username = null;
             protected $password = null;
             protected $mfaCode = null;
 
-            function __construct(array $authArgs, ?array $matches = null) {
+            function __construct(array $authArgs, ?array $matches = null, ?string $mfa = null) {
                 $this->username = $authArgs[0];
 
                 if ($matches) {
@@ -117,15 +132,19 @@ class Authorize {
                 } else {
                     $this->password = $authArgs[1];
                 }
+
+                if($mfa) {
+                    $this->mfaCode = $mfa;
+                }
             }
 
-            public function getUsername() {
+            public function getUsername() : string {
                 return $this->username;
             }
-            public function getPassword() {
+            public function getPassword() : string {
                 return $this->password;
             }
-            public function getMFACode() {
+            public function getMFACode() : ?string {
                 return $this->mfaCode;
             }
         };
